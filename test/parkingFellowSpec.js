5a0c2de394289eca1a496a57c06a92421c074ca2
qu.zhe <qqzh002@gmail.com>
2014-11-02 14:51:06 +0800

    Refactor tests.

--- test/parkingFellowSpec.js
+++ test/parkingFellowSpec.js
@@ -1,23 +1,27 @@
 "use strict";
 
 require("should");
+var given = require("mocha-testdata");
 var ParkingLot = require("../src/parkingLot");
 var ParkingFellow = require("../src/parkingFellow");
+var SmartParkingFellow = require("../src/smartParkingFellow");
 
 describe("Parking Fellow", function () {
 
     describe("when there is only one parking lot", function () {
 
-        it("should be able to park car", function () {
+        given(ParkingFellow, SmartParkingFellow)
+        .it("should be able to park car", function (ParkingFellowConstructor) {
             var parkingLot = new ParkingLot();
-            var parkingFellow = new ParkingFellow([parkingLot]);
+            var parkingFellow = new ParkingFellowConstructor([parkingLot]);
             var ticket = parkingFellow.park("car");
             parkingLot.pick(ticket).should.equal("car");
         });
 
-        it("should be able to pick car", function () {
+        given(ParkingFellow, SmartParkingFellow)
+        .it("should be able to pick car", function (ParkingFellowConstructor) {
             var parkingLot = new ParkingLot();
-            var parkingFellow = new ParkingFellow([parkingLot]);
+            var parkingFellow = new ParkingFellowConstructor([parkingLot]);
             var ticket = parkingLot.park("car");
             parkingFellow.pick(ticket).should.equal("car");
         });
@@ -26,18 +30,20 @@ describe("Parking Fellow", function () {
 
     describe("when there are two parking lots", function () {
 
-        it("should be able to park car when one of the parking lot is full", function () {
+        given(ParkingFellow, SmartParkingFellow)
+        .it("should be able to park car when one of the parking lot is full", function (ParkingFellowConstructor) {
             var fullParkingLot = new ParkingLot(0);
             var emptyParkingLot = new ParkingLot(1);
-            var parkingFellow = new ParkingFellow([fullParkingLot, emptyParkingLot]);
+            var parkingFellow = new ParkingFellowConstructor([fullParkingLot, emptyParkingLot]);
             var ticket = parkingFellow.park("car");
             emptyParkingLot.pick(ticket).should.equal("car");
         });
 
-        it("should be able to pick car", function () {
+        given(ParkingFellow, SmartParkingFellow)
+        .it("should be able to pick car", function (ParkingFellowConstructor) {
             var parkingLot1 = new ParkingLot();
             var parkingLot2 = new ParkingLot();
-            var parkingFellow = new ParkingFellow([parkingLot1, parkingLot2]);
+            var parkingFellow = new ParkingFellowConstructor([parkingLot1, parkingLot2]);
             var ticket1 = parkingLot1.park("car1");
             var ticket2 = parkingLot2.park("car2");
             parkingFellow.pick(ticket1).should.equal("car1");
