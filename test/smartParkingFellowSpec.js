6a41b663f36a19338d5bb2f6da1eaf0f664b6ea1
qu.zhe <qqzh002@gmail.com>
2014-11-11 19:38:49 +0800

    Revert "Refactor tests."
    This reverts commit 5a0c2de394289eca1a496a57c06a92421c074ca2.

--- test/smartParkingFellowSpec.js
+++ test/smartParkingFellowSpec.js
@@ -6,7 +6,43 @@ var SmartParkingFellow = require("../src/smartParkingFellow");
 
 describe("Smart Parking Fellow", function () {
 
+    describe("when there is only one parking lot", function () {
+
+        it("should be able to park car", function () {
+            var parkingLot = new ParkingLot();
+            var parkingFellow = new SmartParkingFellow([parkingLot]);
+            var ticket = parkingFellow.park("car");
+            parkingLot.pick(ticket).should.equal("car");
+        });
+
+        it("should be able to pick car", function () {
+            var parkingLot = new ParkingLot();
+            var parkingFellow = new SmartParkingFellow([parkingLot]);
+            var ticket = parkingLot.park("car");
+            parkingFellow.pick(ticket).should.equal("car");
+        });
+
+    });
+
     describe("when there are two parking lots", function () {
+
+        it("should be able to park car when one of the parking lot is full", function () {
+            var fullParkingLot = new ParkingLot(0);
+            var emptyParkingLot = new ParkingLot(1);
+            var parkingFellow = new SmartParkingFellow([fullParkingLot, emptyParkingLot]);
+            var ticket = parkingFellow.park("car");
+            emptyParkingLot.pick(ticket).should.equal("car");
+        });
+
+        it("should be able to pick car", function () {
+            var parkingLot1 = new ParkingLot();
+            var parkingLot2 = new ParkingLot();
+            var parkingFellow = new SmartParkingFellow([parkingLot1, parkingLot2]);
+            var ticket1 = parkingLot1.park("car1");
+            var ticket2 = parkingLot2.park("car2");
+            parkingFellow.pick(ticket1).should.equal("car1");
+            parkingFellow.pick(ticket2).should.equal("car2");
+        });
 
         it("should be able to park car to the larger one when both are empty", function () {
             var smallerParkingLot = new ParkingLot(1);
